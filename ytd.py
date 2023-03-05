95a74c813991a7b412304ddbf46ffca50bc6dee4
unknown <sraghul360@gmail.com>
2023-03-05 18:09:28 +0530

    working script downloading at 1080

---
+++ ytd.py
@@ -0,0 +1,79 @@
+from yt_dlp import YoutubeDL as ytd
+import sys
+URL = []
+
+def format_selector(ctx):
+    """ Select the best video and the best audio that won't result in an mkv.
+    NOTE: This is just an example and does not handle all cases """
+
+    # formats are already sorted worst to best
+    formats = ctx.get('formats')[::-1]
+
+    # acodec='none' means there is no audio
+    best_video = next(f for f in formats
+                      if f['vcodec'] != 'none' and f['acodec'] == 'none' and f['resolution'] == '1920x1080' and f['ext'] == 'mp4')
+    
+    # find compatible audio extension
+    audio_ext = {'mp4': 'm4a', 'webm': 'webm'}[best_video['ext']]
+
+    # vcodec='none' means there is no video
+    best_audio = next(f for f in formats if (
+        f['acodec'] != 'none' and f['vcodec'] == 'none' and f['ext'] == audio_ext))
+
+    # These are the minimum required fields for a merged format
+    yield {
+        'format_id': f'{best_video["format_id"]}+{best_audio["format_id"]}',
+        'ext': best_video['ext'],
+        'requested_formats': [best_video, best_audio],
+        # Must be + separated list of protocols
+        'protocol': f'{best_video["protocol"]}+{best_audio["protocol"]}'
+    }
+
+class MyLogger:
+    def debug(self, msg):
+        # For compatibility with youtube-dl, both debug and info are passed into debug
+        # You can distinguish them by the prefix '[debug] '
+        if msg.startswith('[debug] '):
+            pass
+        else:
+            self.info(msg)
+
+    def info(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print(d['filename']," done, post processing in progress .......")
+
+
+ydl_opts = {
+    'format': format_selector,
+    #'logger': MyLogger(),
+    #'progress_hooks': [my_hook],
+    #'prepare_outtmpl': custom_prepare_outtmpl,
+    'restrictfilenames': False,
+    #'subtitleslangs': "en",
+    'writeautomaticsub': True,
+    'writesubtitles': True,
+    #'listsubtitles': True,
+    'writedescription': False,
+    'clean_infojson': False,
+    'writeinfojson': False,
+    'windowsfilenames': True,
+    'outtmpl' : "%(playlist_index|)s%(playlist_index& - |)s%(title)s.%(ext)s",
+}
+
+def main(argv):
+    # print ('Number of arguments:', len(argv), 'arguments.')
+    # print ('Argument List:', str(argv))
+    with ytd(ydl_opts) as ydl:
+        ydl.download(argv[0])
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
